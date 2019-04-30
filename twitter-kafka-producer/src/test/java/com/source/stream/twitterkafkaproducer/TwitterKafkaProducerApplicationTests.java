d820a061e736a6b79d8b8f72ff82e6e975eb5e22
Loganathan Murugesan <cresloga@gmail.com>
2019-04-30 09:00:53 +0530

    initial commit

---
+++ twitter-kafka-producer/src/test/java/com/source/stream/twitterkafkaproducer/TwitterKafkaProducerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.source.stream.twitterkafkaproducer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwitterKafkaProducerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
